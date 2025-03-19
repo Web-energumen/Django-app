134efd722d9b31a409dc151a1820f92d14fdddfa
Alex <doroshevsn@gmail.com>
2025-03-19 16:29:21 +0200

    Refactor main app: update templates, URLs, and views

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Home - Головна'
+        context['content'] = 'Магазин меблів HOME'
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['title'] = 'Home - Про нас'
+        context['content'] = 'Про нас'
+        return context
